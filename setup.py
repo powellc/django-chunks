b86bf9c85fa0f1ebbede13b117d68d9fce5afb3f
Colin Powell <colin@onecardinal.com>
2011-04-21 00:35:17 -0400

    Changes for pypi installations.

--- setup.py
+++ setup.py
@@ -1,17 +1,33 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='chunks',
-      version='0.1',
-      description='Keyed blocks of content for use in your Django templates',
-      author='Clint Ecker',
-      author_email='me@clintecker.com',
-      url='http://code.google.com/p/django-chunks/',
-      packages=['chunks', 'chunks.templatetags'],
-      classifiers=['Development Status :: 4 - Beta',
-                   'Environment :: Web Environment',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-      )
+setup(
+    name='django-chunks',
+    version=__import__('chunks').__version__,
+    license="BSD",
+
+    install_requires = ['django-markup-mixin',],
+
+    description='Keyed chunks of conetnt for Django, now with Markup options.',
+    long_description=open('README.md').read(),
+
+    author='Colin Powell',
+    author_email='colin@onecardinal.com',
+
+    url='http://github.com/powellc/django-chunks',
+    download_url='http://github.com/powellc/django-chunks/downloads',
+
+    include_package_data=True,
+
+    packages=['chunks'],
+
+    zip_safe=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ]
+)
